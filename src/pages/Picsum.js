11f2f0e33df1cf9a8beb89b733b969f0e56fbc66
koneriranjith-umusic <RanjithKumar.Koneri@umusic.com>
2020-09-26 10:49:09 +0530

    service call added

--- src/pages/Picsum.js
+++ src/pages/Picsum.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 import { useStore } from "../store";
@@ -8,10 +8,33 @@ import { removeTodo } from "../actions";
 
 const Picsum = () => {
 const [{ todos }, dispatch] = useStore();
+const [imageItems, setImageItems] = useState([]);
 
+useEffect(() => {
+    fetch('https://picsum.photos/v2/list?page=1&limit=10').then((response)=>{
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+    
+          // Examine the text in the response
+          response.json().then(function(data) {
+            console.log(data);
+            setImageItems(data)
+          });
+    })
+}, [])
     return (
         <View style={styles.container}>
           <Text>Picsum test</Text>
+          
+        <View>
+            display images from api
+            {imageItems.map((val, index) => (
+            <View key={index}><Text>{val.author}</Text>
+            </View>))}
+        </View>
           {todos.map((val, index) => (
             <View key={index}><Text>{val.name}</Text>
                 <TouchableHighlight onPress = {() => { dispatch(removeTodo(val)) } }>
