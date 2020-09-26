b19a0fcfe9e0a9550f90857c60c0582fef6274d3
koneriranjith-umusic <RanjithKumar.Koneri@umusic.com>
2020-09-26 10:01:58 +0530

    redux setup done

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StoreProvider } from "./src/store";
+import reducers from "./src/reducers"
+import initialState from "./src/store/initialState"
+
+import Picsum from "./src/pages/Picsum"
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Picsum test</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StoreProvider initialState={initialState} reducer={reducers}>
+      <Picsum />
+    </StoreProvider>
   );
 }
 
---
+++ src/pages/Picsum.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+import { useStore } from "../store";
+import { removeTodo } from "../actions";
+
+
+const Picsum = () => {
+const [{ todos, filter }, dispatch] = useStore();
+
+    return (
+        <View style={styles.container}>
+          <Text>Picsum test</Text>
+          {todos.map((val, index) => (
+              <View key={index}><Text>{val.name}</Text>
+              <TouchableHighlight onPress = {() => { dispatch(removeTodo(val)) } }>
+              <View>
+              <Text>
+              remove
+              </Text>
+              </View>
+          </TouchableHighlight>
+          </View>))}
+          <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default Picsum
