bafaa6b36432b79dd76f6a489968b05a54d9ea64
koneriranjith-umusic <RanjithKumar.Koneri@umusic.com>
2020-09-26 23:00:36 +0530

    shuffle function added

--- src/pages/Picsum.js
+++ src/pages/Picsum.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
 
 import { useStore } from "../store";
 import { removeTodo } from "../actions";
@@ -10,6 +10,10 @@ const Picsum = () => {
 const [{ todos }, dispatch] = useStore();
 const [imageItems, setImageItems] = useState([]);
 
+const shuffle = (array) => {
+   return array.sort(() => Math.random() - 0.5);
+}
+  
 useEffect(() => {
     fetch('https://picsum.photos/v2/list?page=1&limit=10').then((response)=>{
         if (response.status !== 200) {
@@ -21,7 +25,7 @@ useEffect(() => {
           // Examine the text in the response
           response.json().then(function(data) {
             console.log(data);
-            setImageItems(data)
+            setImageItems(shuffle(data))
           });
     })
 }, [])
@@ -30,9 +34,10 @@ useEffect(() => {
           <Text>Picsum test</Text>
           
         <View>
-            display images from api
             {imageItems.map((val, index) => (
             <View key={index}><Text>{val.author}</Text>
+            <Image source={{uri: `https://picsum.photos/200/300?image=${val.id}`}}
+                style={{width: 200, height: 300}} />
             </View>))}
         </View>
           {todos.map((val, index) => (
